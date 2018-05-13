42312ada3963fd1369bcc8c03f2ecbeee9e7ed24
Max Alva <alva85@gmail.com>
2018-05-13 18:14:15 -0500

    Configuracion de sliders, guardando valores en UserDefaults

--- ColoresSlider/ViewController.swift
+++ ColoresSlider/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    var red: Float = 0
+    var green: Float = 0
+    var blue: Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func redAction(_ sender: UISlider) {
+        self.red = sender.value
+        self.changeColor(red: CGFloat(self.red), green: CGFloat(self.green), blue: CGFloat(self.blue))
+        UserDefaults.standard.set(self.red, forKey: "red")
+    }
+    
+    @IBAction func greenAction(_ sender: UISlider) {
+        self.green = sender.value
+        self.changeColor(red: CGFloat(self.red), green: CGFloat(self.green), blue: CGFloat(self.blue))
+        UserDefaults.standard.set(self.green, forKey: "green")
+    }
+    
+    @IBAction func blueAction(_ sender: UISlider) {
+        self.blue = sender.value
+        self.changeColor(red: CGFloat(self.red), green: CGFloat(self.green), blue: CGFloat(self.blue))
+        UserDefaults.standard.set(self.blue, forKey: "blue")
+    }
+    
+    func changeColor(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        view.backgroundColor = UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
+    }
 
 }
 
